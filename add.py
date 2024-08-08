4ff2a2d89bc00c143bc5f83ba09a5ef20d6520a7
irinaalmaeva <almaeva.irina@gmail.com>
2024-08-08 22:47:31 +0300

    add add.py

---
+++ add.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    now = datetime.now()
+    date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    return render_template('time.html', date_time=date_time)
+
+if __name__ == '__main__':
+    app.run(debug=True)
